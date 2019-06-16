b18afbcf92c17b2c995e1cca7098fb20ccc1a3e0
Taiki Endo <te316e89@gmail.com>
2019-06-16 23:15:46 +0900

    Transition to Rust 2018

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,16 @@
 # Unreleased
 
+* Transition to Rust 2018. With this change, the minimum required version will go up to Rust 1.31.
+
+* Removed `"std"` crate feature. This crate can generate accurate code without `"std"` feature.
+
+* Updated minimum `derive_utils` version to 0.7.2.
+
 # 0.1.2 - 2019-02-05
 
-* Update minimum derive_utils version to 0.6.3.
+* Update minimum `derive_utils` version to 0.6.3.
 
-* Update minimum syn version to 0.15.22.
+* Update minimum `syn` version to 0.15.22.
 
 # 0.1.1 - 2019-02-03
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 //! ```rust
 //! # #![cfg_attr(feature = "try_trait", feature(try_trait))]
 //! # #![cfg_attr(feature = "exact_size_is_empty", feature(exact_size_is_empty))]
-//! # extern crate core;
-//! # extern crate iterator_enum;
 //! use iterator_enum::*;
 //!
 //! #[derive(Iterator, DoubleEndedIterator, ExactSizeIterator, FusedIterator, Extend)]
@@ -35,14 +33,7 @@
 //! * [`TrustedLen`](https://doc.rust-lang.org/std/iter/trait.TrustedLen.html) (*requires `"trusted_len"` crate feature*)
 //! * [`Extend`](https://doc.rust-lang.org/std/iter/trait.Extend.html)
 //!
-//! See [this issue](https://github.com/taiki-e/auto_enums/issues/11) for other traits.
-//!
 //! ## Crate Features
-//!
-//! * `std`
-//!   * Enabled by default.
-//!   * Generate code for `std` library.
-//!   * Disable this feature to generate code for `no_std`.
 //!
 //! * `try_trait`
 //!   * Disabled by default.
@@ -62,42 +53,28 @@
 
 #![recursion_limit = "256"]
 #![doc(html_root_url = "https://docs.rs/iterator-enum/0.1.2")]
-#![deny(unsafe_code)]
-#![deny(bare_trait_objects, elided_lifetimes_in_paths, unreachable_pub)]
+#![doc(test(attr(deny(warnings), allow(dead_code, unused_assignments, unused_variables))))]
+#![warn(unsafe_code)]
+#![warn(rust_2018_idioms, unreachable_pub)]
+#![warn(single_use_lifetimes)]
+#![warn(clippy::all, clippy::pedantic)]
+#![warn(clippy::nursery)]
 
-extern crate derive_utils;
 extern crate proc_macro;
-extern crate quote;
-extern crate syn;
 
-use derive_utils::{derive_trait, EnumData as Data, __rt::ident_call_site};
+use derive_utils::{derive_trait, EnumData as Data};
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn::parse_quote;
+use syn::{parse_quote, Ident};
 
-/// Returns standard library's root.
-///
-/// In default returns `::std`.
-/// if disabled default crate feature, returned `::core`.
-macro_rules! std_root {
-    () => {{
-        #[cfg(feature = "std")]
-        {
-            quote!(::std)
-        }
-        #[cfg(not(feature = "std"))]
-        {
-            quote!(::core)
-        }
-    }};
+fn ident<S: AsRef<str>>(s: S) -> Ident {
+    Ident::new(s.as_ref(), Span::call_site())
 }
 
 macro_rules! parse {
     ($input:expr) => {
-        match syn::parse($input)
-            .map_err(derive_utils::Error::from)
-            .and_then(|item| Data::from_derive(&item))
-        {
+        match syn::parse($input).and_then(|item| Data::from_derive(&item)) {
             Ok(data) => data,
             Err(err) => return TokenStream::from(err.to_compile_error()),
         }
@@ -106,16 +83,13 @@ macro_rules! parse {
 
 #[proc_macro_derive(Iterator)]
 pub fn derive_iterator(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     #[cfg(feature = "try_trait")]
     let try_trait = quote! {
         #[inline]
         fn try_fold<__U, __F, __R>(&mut self, init: __U, f: __F) -> __R
         where
-            __F: #root::ops::FnMut(__U, Self::Item) -> __R,
-            __R: #root::ops::Try<Ok = __U>;
+            __F: ::core::ops::FnMut(__U, Self::Item) -> __R,
+            __R: ::core::ops::Try<Ok = __U>;
     };
     // It is equally efficient if `try_fold` can be used.
     #[cfg(not(feature = "try_trait"))]
@@ -123,53 +97,53 @@ pub fn derive_iterator(input: TokenStream) -> TokenStream {
         #[inline]
         fn fold<__U, __F>(self, init: __U, f: __F) -> __U
         where
-            __F: #root::ops::FnMut(__U, Self::Item) -> __U;
+            __F: ::core::ops::FnMut(__U, Self::Item) -> __U;
         #[inline]
         fn all<__F>(&mut self, f: __F) -> bool
         where
-            __F: #root::ops::FnMut(Self::Item) -> bool;
+            __F: ::core::ops::FnMut(Self::Item) -> bool;
         #[inline]
         fn any<__F>(&mut self, f: __F) -> bool
         where
-            __F: #root::ops::FnMut(Self::Item) -> bool;
+            __F: ::core::ops::FnMut(Self::Item) -> bool;
         #[inline]
-        fn find<__P>(&mut self, predicate: __P) -> #root::option::Option<Self::Item>
+        fn find<__P>(&mut self, predicate: __P) -> ::core::option::Option<Self::Item>
         where
-            __P: #root::ops::FnMut(&Self::Item) -> bool;
+            __P: ::core::ops::FnMut(&Self::Item) -> bool;
         #[inline]
-        fn find_map<__U, __F>(&mut self, f: __F) -> #root::option::Option<__U>
+        fn find_map<__U, __F>(&mut self, f: __F) -> ::core::option::Option<__U>
         where
-            __F: #root::ops::FnMut(Self::Item) -> #root::option::Option<__U>;
+            __F: ::core::ops::FnMut(Self::Item) -> ::core::option::Option<__U>;
         #[inline]
-        fn position<__P>(&mut self, predicate: __P) -> #root::option::Option<usize>
+        fn position<__P>(&mut self, predicate: __P) -> ::core::option::Option<usize>
         where
-            __P: #root::ops::FnMut(Self::Item) -> bool;
+            __P: ::core::ops::FnMut(Self::Item) -> bool;
     };
 
     derive_trait!(
-        data,
-        parse_quote!(#root::iter::Iterator),
+        parse!(input),
+        parse_quote!(::core::iter::Iterator),
         parse_quote! {
             trait Iterator {
                 type Item;
                 #[inline]
-                fn next(&mut self) -> #root::option::Option<Self::Item>;
+                fn next(&mut self) -> ::core::option::Option<Self::Item>;
                 #[inline]
-                fn size_hint(&self) -> (usize, #root::option::Option<usize>);
+                fn size_hint(&self) -> (usize, ::core::option::Option<usize>);
                 #[inline]
                 fn count(self) -> usize;
                 #[inline]
-                fn last(self) -> #root::option::Option<Self::Item>;
+                fn last(self) -> ::core::option::Option<Self::Item>;
                 #[inline]
-                fn nth(&mut self, n: usize) -> #root::option::Option<Self::Item>;
+                fn nth(&mut self, n: usize) -> ::core::option::Option<Self::Item>;
                 #[inline]
                 #[must_use = "if you really need to exhaust the iterator, consider `.for_each(drop)` instead"]
-                fn collect<__U: #root::iter::FromIterator<Self::Item>>(self) -> __U;
+                fn collect<__U: ::core::iter::FromIterator<Self::Item>>(self) -> __U;
                 #[inline]
                 fn partition<__U, __F>(self, f: __F) -> (__U, __U)
                 where
-                    __U: #root::default::Default + #root::iter::Extend<Self::Item>,
-                    __F: #root::ops::FnMut(&Self::Item) -> bool;
+                    __U: ::core::default::Default + ::core::iter::Extend<Self::Item>,
+                    __F: ::core::ops::FnMut(&Self::Item) -> bool;
                 #try_trait
             }
         },
@@ -180,16 +154,13 @@ pub fn derive_iterator(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(DoubleEndedIterator)]
 pub fn derive_double_ended_iterator(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     #[cfg(feature = "try_trait")]
     let try_trait = quote! {
         #[inline]
         fn try_rfold<__U, __F, __R>(&mut self, init: __U, f: __F) -> __R
         where
-            __F: #root::ops::FnMut(__U, Self::Item) -> __R,
-            __R: #root::ops::Try<Ok = __U>;
+            __F: ::core::ops::FnMut(__U, Self::Item) -> __R,
+            __R: ::core::ops::Try<Ok = __U>;
     };
     // It is equally efficient if `try_rfold` can be used.
     #[cfg(not(feature = "try_trait"))]
@@ -197,21 +168,21 @@ pub fn derive_double_ended_iterator(input: TokenStream) -> TokenStream {
         #[inline]
         fn rfold<__U, __F>(self, accum: __U, f: __F) -> __U
         where
-            __F: #root::ops::FnMut(__U, Self::Item) -> __U;
+            __F: ::core::ops::FnMut(__U, Self::Item) -> __U;
         #[inline]
-        fn rfind<__P>(&mut self, predicate: __P) -> #root::option::Option<Self::Item>
+        fn rfind<__P>(&mut self, predicate: __P) -> ::core::option::Option<Self::Item>
         where
-            __P: #root::ops::FnMut(&Self::Item) -> bool;
+            __P: ::core::ops::FnMut(&Self::Item) -> bool;
     };
 
     derive_trait!(
-        data,
-        Some(ident_call_site("Item")),
-        parse_quote!(#root::iter::DoubleEndedIterator),
+        parse!(input),
+        Some(ident("Item")),
+        parse_quote!(::core::iter::DoubleEndedIterator),
         parse_quote! {
-            trait DoubleEndedIterator: #root::iter::Iterator {
+            trait DoubleEndedIterator: ::core::iter::Iterator {
                 #[inline]
-                fn next_back(&mut self) -> #root::option::Option<Self::Item>;
+                fn next_back(&mut self) -> ::core::option::Option<Self::Item>;
                 #try_trait
             }
         },
@@ -222,9 +193,6 @@ pub fn derive_double_ended_iterator(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(ExactSizeIterator)]
 pub fn derive_exact_size_iterator(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     #[cfg(not(feature = "exact_size_is_empty"))]
     let is_empty = quote!();
     #[cfg(feature = "exact_size_is_empty")]
@@ -234,11 +202,11 @@ pub fn derive_exact_size_iterator(input: TokenStream) -> TokenStream {
     };
 
     derive_trait!(
-        data,
-        Some(ident_call_site("Item")),
-        parse_quote!(#root::iter::ExactSizeIterator),
+        parse!(input),
+        Some(ident("Item")),
+        parse_quote!(::core::iter::ExactSizeIterator),
         parse_quote! {
-            trait ExactSizeIterator: #root::iter::Iterator {
+            trait ExactSizeIterator: ::core::iter::Iterator {
                 #[inline]
                 fn len(&self) -> usize;
                 #is_empty
@@ -251,15 +219,12 @@ pub fn derive_exact_size_iterator(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(FusedIterator)]
 pub fn derive_fused_iterator(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     derive_trait!(
-        data,
-        Some(ident_call_site("Item")),
-        parse_quote!(#root::iter::FusedIterator),
+        parse!(input),
+        Some(ident("Item")),
+        parse_quote!(::core::iter::FusedIterator),
         parse_quote! {
-            trait FusedIterator: #root::iter::Iterator {}
+            trait FusedIterator: ::core::iter::Iterator {}
         },
     )
     .unwrap_or_else(|e| e.to_compile_error())
@@ -269,15 +234,12 @@ pub fn derive_fused_iterator(input: TokenStream) -> TokenStream {
 #[cfg(feature = "trusted_len")]
 #[proc_macro_derive(TrustedLen)]
 pub fn derive_trusted_len(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     derive_trait!(
-        data,
-        Some(ident_call_site("Item")),
-        parse_quote!(#root::iter::TrustedLen),
+        parse!(input),
+        Some(ident("Item")),
+        parse_quote!(::core::iter::TrustedLen),
         parse_quote! {
-            unsafe trait TrustedLen: #root::iter::Iterator {}
+            unsafe trait TrustedLen: ::core::iter::Iterator {}
         },
     )
     .unwrap_or_else(|e| e.to_compile_error())
@@ -286,16 +248,13 @@ pub fn derive_trusted_len(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(Extend)]
 pub fn derive_extend(input: TokenStream) -> TokenStream {
-    let data = parse!(input);
-    let root = std_root!();
-
     derive_trait!(
-        data,
-        parse_quote!(#root::iter::Extend),
+        parse!(input),
+        parse_quote!(::core::iter::Extend),
         parse_quote! {
             trait Extend<__A> {
                 #[inline]
-                fn extend<__T: #root::iter::IntoIterator<Item = __A>>(&mut self, iter: __T);
+                fn extend<__T: ::core::iter::IntoIterator<Item = __A>>(&mut self, iter: __T);
             }
         },
     )
