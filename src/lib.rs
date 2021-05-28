1f53d04f018d6fe29648947412785a9207d2174a
Taiki Endo <te316e89@gmail.com>
2021-05-28 22:09:07 +0900

    Apply clippy::wildcard_imports lint

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@
 ))]
 #![forbid(unsafe_code)]
 #![warn(future_incompatible, rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
-#![warn(clippy::default_trait_access)]
+#![warn(clippy::default_trait_access, clippy::wildcard_imports)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 // older compilers require explicit `extern crate`.
